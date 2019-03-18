3bdc5fa6fba2829efa67e5763af4386662602a81
mjzh <1174120452@qq.com>
2019-03-19 07:48:09 +0800

    hystrix断路器

--- microservice-eureka-consumer/src/main/java/com/example/service/HelloService.java
+++ microservice-eureka-consumer/src/main/java/com/example/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,10 +17,16 @@ public class HelloService {
 
     /**
      * 测试接口联通性
+     * HystrixCommand创建熔断器功能,并指定fallbackMethod方法
      * @param name
      * @return
      */
+    @HystrixCommand(fallbackMethod = "testError")
     public String test(String name) {
         return restTemplate.getForObject("http://MICROSERVICE-EUREKA-CLIENT/hi?name="+name, String.class);
+    }
+
+    public String testError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 }
