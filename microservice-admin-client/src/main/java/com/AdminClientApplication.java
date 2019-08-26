96f5be9a554fbfbe5588fbc67b0fb0ba33b2dea1
mjzh <1174120452@qq.com>
2019-08-26 21:52:13 +0800

    spring boot admin与eureka监控所有服务

--- microservice-admin-client/src/main/java/com/AdminClientApplication.java
+++ microservice-admin-client/src/main/java/com/AdminClientApplication.java
@@ -2,15 +2,16 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author zhou
  * @date 2019/8/18 20:53
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminClientApplication.class, args);
     }
-
 }
